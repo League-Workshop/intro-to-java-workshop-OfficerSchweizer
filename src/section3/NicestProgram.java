151e9707864297b5d6087bfc7146ef503e550f7c
League <league@WTS8.attlocal.net>
2018-08-08 12:02:30 -0700

    day 3 coding

---
+++ src/section3/NicestProgram.java
@@ -0,0 +1,13 @@
+package section3;
+
+import javax.swing.JOptionPane;
+
+public class NicestProgram {
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "Your Nike shirt is nice");
+		JOptionPane.showMessageDialog(null, "I like your shoes");
+		JOptionPane.showMessageDialog(null, "You are a good programmer");
+		
+		
+	}
+}
--- src/section3/WorldDomination.java
+++ src/section3/WorldDomination.java
@@ -3,14 +3,18 @@ package section3;
 import javax.swing.JOptionPane;
 
 public class WorldDomination {
-	
+
 	public static void main(String[] args) {
 		// 1. Ask the user if they know how to write code.
-
+		String yesNo = JOptionPane.showInputDialog("Do you know how to write code?");
 		// 2. If they say "yes", tell them they will rule the world.
+		if (yesNo.equals("yes")) {
+			JOptionPane.showMessageDialog(null, "You will eventually rule the world.");
 
-		// 3. Otherwise, wish them good luck washing dishes.
+		} else {
+			JOptionPane.showMessageDialog(null, "Good luck washing dishes for the rest of your life");
+			// 3. Otherwise, wish them good luck washing dishes.
 
+		}
 	}
 }
-
